16f978213454b38283373e8ba7587a654dfd3d5b
Fernando Larrosa <fernando.larrosa94@gmail.com>
2022-03-30 17:37:56 -0300

    configurando db_uri

--- database.js
+++ database.js
@@ -4,6 +4,7 @@ exports.connectDB = void 0;
 const mongoose_1 = require("mongoose");
 const DB_URI = process.env.DB_URI;
 const connectDB = () => {
+    console.log(`DIRECCION DE BASE DE DATOS ${DB_URI}`);
     if (!DB_URI)
         return console.log('no existe direccion de base de datos');
     (0, mongoose_1.connect)(DB_URI, () => {
